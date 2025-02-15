2504b2f381382e0b7047e99b68a6357e6a2492b0
Jules Charlier <jules.charlier123@gmail.com>
2025-02-15 16:47:35 +0100

    +1300 pts !!

--- B_New_Skateboard.py
+++ B_New_Skateboard.py
@@ -1,2 +1,12 @@
 nombre = input()
+n = 0
 
+for i in range(len(nombre)-1):
+    if int((nombre[i:i+2]))%4 == 0:
+        n += i + 1
+
+for chiffre in nombre:
+    if int(chiffre)%4 == 0:
+        n +=1
+        
+print(n)
