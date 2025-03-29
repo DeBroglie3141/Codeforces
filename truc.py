3f915ff0aab87375b9b5eb520ef1f63e71b65a32
Jules Charlier <jules.charlier123@gmail.com>
2025-03-29 19:17:38 +0100

    root me pro !

--- truc.py
+++ truc.py
@@ -1,3 +1,21 @@
-nums = [4, 3, 2, 1]
-nums.sort(reverse=False)
-print(nums)
+import http.client
+
+conn = http.client.HTTPSConnection("http-first-steps.challenges.pro.root-me.org")
+
+headers = {
+    "0": "pineapple",
+    "1": "1",
+    "2": "2",
+    "3": "3",
+    "4": "4",
+    "5": "5",
+    "6": "6",
+    "7": "7",
+    "8": "pizza"
+}
+
+conn.request("GET", "/", headers=headers)
+
+response = conn.getresponse()
+print(response.status, response.read().decode())
+conn.close()
